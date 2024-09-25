3040a7defc25f5448ed1b1a78f5e17c08c864d30
rungelea <88401308+rungelea@users.noreply.github.com>
2024-09-25 23:22:43 +0200

    Update try_new_preprocess.R

--- try_new_preprocess.R
+++ try_new_preprocess.R
@@ -17,3 +17,22 @@ dta_est<-preprocess(data_age_birth, var_vec = c(1,3),
 
 
 # mixed case nur vektor bin_width
+?preprocess
+
+
+dta <-data.frame(
+  obs_density = sample(0:2, 100, replace = TRUE, prob = c(0.15, 0.1, 0.75)),
+  covariate1 = sample(c("a", "b"), 100, replace = TRUE),
+  covariate2 = sample(c("c", "d"), 100, replace = TRUE),
+  sample_weights = runif(100, 0, 2)
+)
+dta[which(dta$obs_density == 2), ]$obs_density <- rbeta(length(which(dta$obs_density == 2)), shape1 = 3, shape2 = 3)
+p<- preprocess(
+  dta,
+  var_vec = c("covariate1", "covariate2"),
+  density_var = "obs_density",
+  sample_weights = "sample_weights",
+  bin_number = 10
+)
+
+a<-preprocess(p, var_vec="covariate1", density_var = "obs_density", already_formatted = TRUE)
