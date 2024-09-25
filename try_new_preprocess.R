d4dd0eace54870d0579670468a3229c2da576bf6
rungelea <88401308+rungelea@users.noreply.github.com>
2024-09-25 15:41:37 +0200

    Update try_new_preprocess.R

--- try_new_preprocess.R
+++ try_new_preprocess.R
@@ -3,12 +3,15 @@ load_all("C:/Users/learu/CondDensReg/new/CondDensReg")
 library(dplyr)
 
 debug(preprocess)
-dta_est<-preprocess(data_age_birth, var_vec = c(1,3),
+
+dta<-data_age_birth
+dta$weighted_counts<-dta$counts*2
+dta_est<-preprocess(dta, var_vec = c(1,3),
                     density_var = 2,
-                    domain_continuous =15:50,
-                    values_discrete = 16.5,
+                    domain_continuous =FALSE,
+                    values_discrete = 15:49+0.5,
                     bin_width = 15:49,
-                    weights_discrete =c(20),
+                    weights_discrete =2,
                     already_formatted = TRUE)
 
 
