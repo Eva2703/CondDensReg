2e1bb08dbe817c756efd7bc2f308ee5bfb8c9c65
rungelea <88401308+rungelea@users.noreply.github.com>
2024-09-25 09:10:24 +0200

    Update try_new_preprocess.R

--- try_new_preprocess.R
+++ try_new_preprocess.R
@@ -5,7 +5,11 @@ library(dplyr)
 debug(preprocess)
 dta_est<-preprocess(data_age_birth, var_vec = c(1,3),
                     density_var = 2,
-                    domain_continuous = c(15,50),
-                    values_discrete = FALSE,
-                   # bin_width = 1,
+                    domain_continuous =15:50,
+                    values_discrete = 16.5,
+                    bin_width = 15:49,
+                    weights_discrete =c(20),
                     already_formatted = TRUE)
+
+
+# mixed case nur vektor bin_width
